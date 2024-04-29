3819f972c228c611dbd7a874fab42cec45bf9115
brunaballerini <brunaballerini@gmail.com>
2024-04-29 10:00:45 -0300

    Atualizacao do arquivo teste_maior_menor.rb acrescentando mostrar chutes_antigos

--- jogo_adivinhacao/maior_menor.rb
+++ jogo_adivinhacao/maior_menor.rb
@@ -7,17 +7,16 @@ def da_boa_vindas
   puts "Começando o jogo para você: #{nome_jogador} \n"
 end
 
-
 def sorteia_numero_secreto
   numero_secreto_sorteado = 175
+  return numero_secreto_sorteado
 end
-
 
 def pede_um_numero numero_da_jogada
   puts "Tentativa #{numero_da_jogada.to_s}: Digite o número para chute\n"
   chute = gets.to_i
+  return chute
 end
-
 
 def verifica_se_acertou numero_sorteado, chute_do_jogador
  acertou = numero_sorteado == chute_do_jogador
@@ -34,17 +33,17 @@ def verifica_se_acertou numero_sorteado, chute_do_jogador
   end
 end
 
-
 da_boa_vindas
 numero_secreto = sorteia_numero_secreto
-jogada = 1
 numero_de_jogadas = 5
-while jogada < numero_de_jogadas
+chutes_antigos = []
+for jogada in 1..numero_de_jogadas
 
   chute = pede_um_numero jogada
+  chutes_antigos.append(chute)
 
   break if verifica_se_acertou numero_secreto, chute
 
-  jogada = jogada + 1
+  puts "Chutes: #{chutes_antigos.to_s}"
 
 end
--- jogo_adivinhacao/teste_maior_menor.rb
+++ jogo_adivinhacao/teste_maior_menor.rb
@@ -8,7 +8,7 @@ puts 27 < 175
 puts 27 >= 175
 puts "=============="
 numero = 0
-while numero < 2
+while numero < 5
   puts numero
   numero = numero + 1
 end
@@ -16,4 +16,22 @@ puts "=============="
 for i in 1..3
   puts "contando " + i.to_s
 end
+puts "=============="
+chutes_antigos = [176, 130, 150, 175]
+puts chutes_antigos[1]
+for chute in chutes_antigos
+  puts "Chutes: " + chute.to_s
+end
+puts "=============="
+chutes_antigos.append(190)
+chutes_antigos << 10
+puts "=============="
+puts "Chutes: #{chutes_antigos.to_s}"
+puts "=============="
+nome = "                      bruna ballerini          "
+puts nome.size
+nome_melhorado = nome.strip
+puts nome_melhorado.size
+nome_chomp = nome.chomp
+puts nome_chomp.size
 puts "=============="
