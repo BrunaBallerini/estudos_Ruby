33529e1af112d4c84a43304eaeca375407af1748
brunaballerini <brunaballerini@gmail.com>
2024-05-06 09:35:32 -0300

    Alterando a logica do jogo para funcionar corretamente

--- jogo_forca/forca.rb
+++ jogo_forca/forca.rb
@@ -1,7 +1,7 @@
 def da_boa_vindas
   puts "JOGO DE ADIVINHAÇÃO"
   puts "Digite seu nome:\n"
-  nome_jogador = gets.capitalize!
+  nome_jogador = gets.capitalize.chomp
   puts "Começando o jogo para você: #{nome_jogador}"
 end
 
@@ -17,46 +17,69 @@ end
 
 def pede_um_letra
   puts "Digite uma letra:\n"
-  chute = gets.strip!
+  chute = gets.strip.chomp.downcase
   return chute
-end
-
-def verifica_se_acertou palavra_secreta, letra_chute, pontuacao
-  array_formando_palavra = []
-  contador = 0
-  palavra_secreta.each_char do |letra|
-    if letra == letra_chute
-      puts "#{letra} na posição #{contador}"
-    else
-      puts "não tem essa letra"
-    end
-    contador += 1
-    puts array_formando_palavra
-    array_formando_palavra[contador] = letra
-  end
 end
 
 def quer_jogar_novamente quer_jogar
   puts "Você quer jogar novamente? (s/n)"
-  resposta = gets.strip!
-  if resposta == "s" or resposta == "S"
+  resposta = gets.strip.capitalize
+  if resposta == "S"
     quer_jogar = true
+    enforcou = false
+    acertou = false
   else
     quer_jogar = false
   end
-  return quer_jogar
+  return quer_jogar, enforcou, acertou
 end
 
-da_boa_vindas
+enforcou = false
+acertou = false
+pontuacao = 0
+simbolo = "*"
+erros = 0
 quer_jogar = true
+
+da_boa_vindas
 while quer_jogar
-  pontuacao = 1000
   pontuacao_inicial pontuacao
   palavra_secreta = define_palavra_secreta
+  array_formando_palavra = [simbolo] * palavra_secreta.size
 
-  letra_chute = pede_um_letra
-  verifica_se_acertou palavra_secreta, letra_chute, pontuacao
+  while !enforcou && !acertou
+    acertou_letra = false
+    letra_chute = pede_um_letra
 
-  puts "Pontos finais: #{pontuacao}"
-  quer_jogar = quer_jogar_novamente quer_jogar
+    contador = 0
+    palavra_secreta.each_char do |letra|
+      acertou_chute = letra == letra_chute
+      if acertou_chute
+        array_formando_palavra[contador] = letra
+        acertou_letra = true
+        pontuacao += 10
+      end
+      contador += 1
+    end
+
+    if !acertou_letra
+      erros += 1
+      puts "Você errou a letra. Tente novamente"
+    end
+
+    puts "Array: #{array_formando_palavra}"
+    puts "Erros: #{erros}"
+
+    if !array_formando_palavra.include?(simbolo)
+      acertou = true
+      puts "Parabens! Pontuação: #{pontuacao}"
+      break
+    elsif erros == 6
+      enforcou = true
+      puts "Não conseguiu! Pontuação #{pontuacao}"
+      break
+    end
+  end
+
+  quer_jogar, enforcou, acertou = quer_jogar_novamente quer_jogar
 end
---
+++ jogo_forca/teste_forca.rb
@@ -0,0 +1,68 @@
+palavra = "estudo"
+palavra.each_char do |letra|
+  puts letra
+end
+puts "=============================="
+array_palavra = []
+index = 0
+for letra in palavra.chars
+  array_palavra[index] = letra
+  index += 1
+end
+print "#{array_palavra}\n"
+puts "=============================="
+array_ = palavra.chars
+print "#{array_}\n"
+puts "=============================="
+array_formando_palavra = []
+index = 0
+for _ in (0...palavra.size)
+  array_formando_palavra[index] = "*"
+  index += 1
+end
+print "#{array_formando_palavra}\n"
+puts "=============================="
+lista = ["a", "b", "c"]
+letra = "d"
+if not lista.include?(letra)
+  puts "#{letra} não está na lista."
+end
+if !lista.include?(letra)
+  puts "#{letra} não está na lista."
+end
+unless lista.include?(letra)
+  puts "#{letra} não está na lista."
+end
+puts "=============================="
+array_formando_palavra = ["2", "6", "4"]
+simbolo = "*"
+if not array_formando_palavra.include?("*")
+  puts "#{simbolo} não está na lista."
+else
+  puts "#{simbolo} está na lista."
+end
+puts "=============================="
+def verifica_se_acertou array_formando_palavra, acertou_letra, palavra_secreta, letra_chute, pontuacao
+  contador = 0
+  palavra_secreta.each_char do |letra|
+    acertou_chute = letra == letra_chute
+    if acertou_chute
+      array_formando_palavra[contador] = letra
+      acertou_letra = true
+      pontuacao += 10
+    end
+    contador += 1
+  end
+return array_formando_palavra, acertou_letra
+end
+
+array_formando_palavra = ["*", "*", "*", "*", "*", "*"]
+acertou_letra = false
+palavra_secreta = "estudo"
+letra_chute = "t"
+pontuacao = 0
+array, acerto = verifica_se_acertou array_formando_palavra, acertou_letra, palavra_secreta, letra_chute, pontuacao
+
+puts "array: #{array}"
+puts "acerto: #{acerto}"
+puts "=============================="
