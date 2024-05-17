2854a082ffa3ab8d216b3a4a5912dcaf19f71b68
brunaballerini <brunaballerini@gmail.com>
2024-05-17 10:28:43 -0300

    Cria uma classe tipo Estoque para controlar informacoes referentes a estoque

--- basico/basico2.rb
+++ basico/basico2.rb
@@ -1,3 +1,5 @@
+# Anotação
+# puts _____.kind_of? Array
 class Livro
   attr_reader :titulo, :preco, :ano_lancamento
   def initialize (titulo, preco, ano_lancamento,possui_reimpressao)
@@ -20,26 +22,54 @@ class Livro
 
 end
 
+class Estoque
+  attr_reader :livros
+  def initialize
+    @livros = []
+  end
+
+  def livro_para_newsletter
+    @livros.each do |livro|
+      if livro.ano_lancamento < 2000
+        puts "Newsletter -> #{livro.titulo} -> R$ #{livro.preco} - #{livro.possui_reimpressao}"
+      end
+    end
+  end
+
+  def imprime_nota_fiscal
+    @livros.each do |livro|
+      puts "Titulo: #{livro.titulo} - #{livro.preco}"
+    end
+  end
+
+  def livros_mais_baratos_que(valor)
+    @livros.select {|livro| livro.preco <= valor}
+  end
+
+  def total
+    puts "Estoque total: #{@livros.size}"
+  end
+
+  def adiciona_livro (livro)
+    @livros << livro
+  end
+
+end
+
 livro_rails = Livro.new("Agile Web Development with Rails", 70.00,  1999, true)
 livro_ruby = Livro.new("Programming Ruby 1.9", 60.00, 2010, false)
 
-livros = [livro_rails, livro_ruby]
+estoque = Estoque.new
+estoque.adiciona_livro(livro_rails)
+estoque.adiciona_livro(livro_ruby)
+estoque.adiciona_livro(Livro.new("The Pragmatic Programmer", 100, 2015, true))
 
+estoque.imprime_nota_fiscal
+estoque.livro_para_newsletter
+estoque.total
 
-def livro_para_newsletter(livros)
-  livros.each do |livro|
-    if livro.ano_lancamento < 2000
-      puts "Newsletter -> #{livro.titulo} -> R$ #{livro.preco} - #{livro.possui_reimpressao}"
-    end
-  end
+barato = estoque.livros_mais_baratos_que(80)
+puts "Livros mais baratos que 80,00:"
+barato.each do |livro|
+  puts livro.titulo
 end
-
-
-def imprime_nota_fiscal (livros)
-  livros.each do |livro|
-    puts "Titulo: #{livro.titulo} - #{livro.preco}"
-  end
-end
-
-imprime_nota_fiscal(livros)
-livro_para_newsletter(livros)
