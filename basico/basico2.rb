d9895a6eb2445750387389bdcd2ee3982e83e7ba
brunaballerini <brunaballerini@gmail.com>
2024-05-17 13:11:34 -0300

    Adicionando expressao regular para mostrar formato de dinheiro e funcoes relacionadas a vendas

--- basico/basico2.rb
+++ basico/basico2.rb
@@ -5,15 +5,22 @@ module Conversor
   def string_para_alfanumerico(titulo)
     titulo.gsub(/[^\w\s]/,'')
   end
+
+  def string_formato_dinheiro (preco)
+    valor = "R$ #{preco.to_s.tr(".", ",")}"
+    valor << "0" unless valor.match (/(.*)(\d{2})$/)
+    valor
+  end
 end
 
 class Livro
-  attr_reader :titulo, :preco, :ano_lancamento
-  def initialize (titulo, preco, ano_lancamento,possui_reimpressao)
+  attr_reader :titulo, :preco, :ano_lancamento, :editora
+  def initialize (titulo, preco, ano_lancamento,possui_reimpressao, editora = nil)
     @titulo = titulo.extend(Conversor).string_para_alfanumerico(titulo)
     @ano_lancamento = ano_lancamento
-    @preco = calcula_preco(preco)
+    @preco = String.new(calcula_preco(preco).to_s).extend(Conversor).string_formato_dinheiro(preco)
     @possui_reimpressao = possui_reimpressao
+    @editora = editora
   end
 
   def possui_reimpressao
@@ -21,7 +28,7 @@ class Livro
   end
 
   def to_csv
-    return "Titulo: #{@titulo} - #{@preco}"
+    return "Titulo: #{@titulo} - Ano de lançamento: #{@ano_lancamento} - Preço: #{@preco} - Editora: #{@editora}"
   end
 
   private
@@ -45,6 +52,7 @@ class Estoque
   attr_reader :maximo_necessario
   def initialize
     @livros = []
+    @vendas = []
   end
 
   def livro_para_newsletter
@@ -73,8 +81,26 @@ class Estoque
     end
   end
 
-  def remove_livros(livro)
+  def vende(livro)
     @livros.delete(livro)
+    @vendas << livro
+  end
+
+  def vendas_realizadas
+    puts "Vendas realizadas:"
+    @vendas.each do |livro_vendido|
+      puts "#{livro_vendido.to_csv}"
+    end
+  end
+
+  def quantidade_vendas(produto)
+    @vendas.count {|venda| venda.titulo == produto.titulo}
+    # @vendas.each do |venda|
+    #   if venda.titulo == produto.titulo
+    #     quant = @vendas.count
+    #   end
+    #   return quant
+    # end
   end
 
   def maximo_necessario
@@ -83,8 +109,8 @@ class Estoque
 
 end
 
-livro_rails = Livro.new("Agile Web Development with Rails", 70.00,  1999, true)
-livro_ruby = Livro.new("Programming Ruby 1.9", 60.00, 2010, false)
+livro_rails = Livro.new("Agile Web Development with Rails", 70.00,  1999, true, "Rocco")
+livro_ruby = Livro.new("Programming Ruby 1.9", 60.00, 2010, false, "Record")
 livro_programador = Livro.new("The Pragmatic Programmer", 100, 2015, true)
 
 estoque = Estoque.new
@@ -94,6 +120,9 @@ estoque.adiciona_livro(livro_programador)
 
 estoque.imprime_estoque
 estoque.livro_para_newsletter
-estoque.remove_livros(livro_rails)
+estoque.vende(livro_rails)
 estoque.total_atual
 estoque.maximo_necessario
+
+estoque.vendas_realizadas
+puts estoque.quantidade_vendas(livro_rails)
